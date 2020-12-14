472ed9b0f79253b8508ad9596c4630f3ef9f25d3
Brian Broll <brian.broll@gmail.com>
2020-12-14 11:36:38 -0600

    Update schematic first

--- add-error-info.py
+++ add-error-info.py
@@ -7,6 +7,9 @@ from os import path
 import itertools
 script_dir = path.dirname(path.realpath(__file__))
 
+def local_file(filename):
+    return path.join(script_dir, filename)
+
 def spawn(cmds):
     try:
         result = subprocess.run(cmds, timeout=5, capture_output=True)
@@ -20,6 +23,11 @@ def fetch_repo(url):
     spawn(['git', 'clone', '--recurse-submodules', url, work_dir.name])
     return work_dir
 
+def update_schematic(filename):
+    print(f'updating {filename}', file=sys.stderr)
+    cmds = ['bash', local_file('update-eeschema.sh'), filename]
+    spawn(cmds)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('errorfile', help='Error file generated from fetch-netlists-from', type=argparse.FileType('r'), nargs='?')
@@ -28,10 +36,15 @@ if __name__ == '__main__':
     inputs = args.errorfile if args.errorfile is not None else sys.stdin
     infos = ( json.loads(line) for line in inputs )
     for (repo, infos) in itertools.groupby(infos, lambda info: info['repo_url']):
+        infos = list(infos)
         with fetch_repo(repo) as work_dir:
             for info in infos:
                 filename = work_dir + info['schematic']
+                update_schematic(filename)
+
+            for info in infos:
+                filename = work_dir + info['schematic']
                 print(f'checking {filename}', file=sys.stderr)
-                dialog_name = spawn(['bash', path.join(script_dir, 'print-dialog-name.sh'), filename])
+                dialog_name = spawn(['bash', local_file('print-dialog-name.sh'), filename])
                 info['dialog_name'] = dialog_name.strip()
                 print(json.dumps(info))
